6a76fad6345e684a3e92c858c7be583ea4711767
Morshed-17 <morshedmto@gmail.com>
2023-09-18 14:27:36 +0600

    phones bar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import LineChart from "./components/LineChart/LineChart";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
+import Phones from "./components/Phones/Phones";
 
 function App() {
   return (
     <div className="max-w-screen-xl mx-auto md:px-12 px-5">
-      <NavBar/>
-      <PriceOptions/>
-      <LineChart/>
+      <NavBar />
+      <PriceOptions />
+      <div className="mt-6">
+        <LineChart />
+      </div>
+      <Phones/>
     </div>
   );
 }
--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,29 +1,54 @@
-import { LineChart as Lchart, Line , XAxis, YAxis} from 'recharts';
+import {
+  BarChart,
+  LineChart as Lchart,
+  Line,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Bar
+} from "recharts";
 const LineChart = () => {
-   const mathMarks = [
-    {"id": 1, "name": "John Smith", "math": 90, "physics": 85, "chemistry": 78},
-    {"id": 2, "name": "Alice Johnson", "math": 85, "physics": 88, "chemistry": 92},
-    {"id": 3, "name": "Michael Brown", "math": 78, "physics": 75, "chemistry": 80},
-    {"id": 4, "name": "Emily Davis", "math": 44, "physics": 90, "chemistry": 82},
-    {"id": 5, "name": "David Wilson", "math": 88, "physics": 85, "chemistry": 95},
-    {"id": 6, "name": "Sarah Lee", "math": 75, "physics":50, "chemistry": 70},
-    {"id": 7, "name": "Robert Clark", "math": 96, "physics": 88, "chemistry": 91},
-    {"id": 8, "name": "Jessica Hall", "math": 84, "physics": 89, "chemistry": 76},
-    {"id": 9, "name": "Matthew Turner", "math": 77, "physics": 81, "chemistry": 85},
-    {"id": 10, "name": "Olivia Adams", "math": 45, "physics": 55, "chemistry": 49}
-      ]
-    return (
-        <div>
-            <Lchart
-                width={500} height={400} data={mathMarks}>
-                <XAxis></XAxis>
-                <YAxis></YAxis>
-                <Line dataKey="math" stroke='red'/>
-                <Line dataKey="physics" stroke='orange'/>
-                <Line dataKey="chemistry" stroke='blue'/>
-            </Lchart>
-        </div>
-    );
+  const mathMarks = [
+    { id: 1, name: "John", math: 90, physics: 85, chemistry: 78 },
+    { id: 2, name: "Alice", math: 85, physics: 88, chemistry: 92 },
+    { id: 3, name: "Michael", math: 78, physics: 75, chemistry: 80 },
+    { id: 4, name: "Emily", math: 44, physics: 90, chemistry: 82 },
+    { id: 5, name: "David", math: 88, physics: 85, chemistry: 95 },
+    { id: 6, name: "Sarah", math: 75, physics: 50, chemistry: 70 },
+    { id: 7, name: "Robert", math: 96, physics: 88, chemistry: 91 },
+    { id: 8, name: "Jessica", math: 84, physics: 89, chemistry: 76 },
+    { id: 9, name: "Matthew", math: 77, physics: 81, chemistry: 85 },
+    { id: 10, name: "Olivia", math: 95, physics: 85, chemistry: 89 },
+  ];
+
+  return (
+    <div className="grid grid-cols-2">
+      <ResponsiveContainer width="100%" height={400}>
+        <Lchart width={500} data={mathMarks}>
+          <XAxis dataKey="name"></XAxis>
+          <YAxis></YAxis>
+          <Line dataKey="math" stroke="red" />
+          <Line dataKey="physics" stroke="orange" />
+          <Line dataKey="chemistry" stroke="blue" />
+        </Lchart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart width={730} data={mathMarks}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="math" fill="#8884d8" />
+          <Bar dataKey="physics" fill="#82ca9d" />
+          <Bar dataKey="chemistry" fill="#8804d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default LineChart;
